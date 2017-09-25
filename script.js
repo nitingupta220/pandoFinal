b281355deb892f0fb4ef9e43dfba0d35139616cc
nitingupta220 <nitin16@navgurukul.org>
2017-09-25 17:17:03 +0530

    modify

--- script.js
+++ script.js
@@ -16,6 +16,27 @@
 
             });
         $scope.list1 = [1, 2, 3, 4];
+        $scope.selected = {};
+        // gets the template to ng-include for a table row / item
+        $scope.getTemplate = function (data) {
+            if (data.plp_id === $scope.selected.plp_id) return 'edit';
+            else return 'display';
+        };
+
+        $scope.editLogorg = function (data) {
+            $scope.selected = angular.copy(data);
+        };
+
+        $scope.saveLogorg = function (idx) {
+            $scope.mydata[idx] = angular.copy($scope.selected);
+            $scope.reset();
+        };
+
+        $scope.reset = function () {
+            $scope.selected = {};
+        };
+
+
 
     }]);
 
@@ -31,8 +52,6 @@
         };
     });
 })(window.angular);
-
-
 
 //angular code for consignee
 (function (angular) {
@@ -64,6 +83,25 @@
             } else {
                 $scope.orderToDisplay = $scope.data.length;
             }
+        };
+
+        $scope.selected = {};
+        $scope.getTemplate = function (data) {
+            if (data.consignee_id === $scope.selected.consignee_id) return 'edit';
+            else return 'display';
+        };
+
+        $scope.editConsignee = function (data) {
+            $scope.selected = angular.copy(data);
+        };
+
+        $scope.saveConsignee = function (idx) {
+            $scope.mydata[idx] = angular.copy($scope.selected);
+            $scope.reset();
+        };
+
+        $scope.reset = function () {
+            $scope.selected = {};
         };
     }]);
 
@@ -100,8 +138,133 @@
 
 
             });
+        $scope.selected = {};
+        // gets the template to ng-include for a table row / item
+        $scope.getTemplate = function (data) {
+            if (data.vendor_id === $scope.selected.vendor_id) return 'edit';
+            else return 'display';
+        };
+
+        $scope.editVendor = function (data) {
+            $scope.selected = angular.copy(data);
+        };
+
+        $scope.saveVendor = function (idx) {
+            $scope.mydata[idx] = angular.copy($scope.selected);
+            $scope.reset();
+        };
+
+        $scope.reset = function () {
+            $scope.selected = {};
+        };
 
     }]);
+})(window.angular);
+
+//angular code for rate_matrix
+(function (angular) {
+    'use strict';
+    var app = angular.module("ratematrix", []);
+
+    app.controller("rateMatrix", ["$scope", "$http", function ($scope, $http) {
+        //    here $http get a information from "logs_with_gates.json"     
+
+        $http.get("rate_matrix.json")
+            //        then we will call then mathod to call function in which response argument gives all data contain in records as an Array 
+            .then(function (response) {
+
+                $scope.mydata = response.data;
+
+
+            });
+
+    }]);
+    app.controller("detentionMatrix", ["$scope", "$http", function ($scope, $http) {
+        //    here $http get a information from "logs_with_gates.json"     
+
+        $http.get("detention.json")
+            //        then we will call then mathod to call function in which response argument gives all data contain in records as an Array 
+            .then(function (response) {
+
+                $scope.mydata = response.data;
+
+
+            });
+
+    }]);
+})(window.angular);
+
+
+
+
+
+
+
+
+//angular code for Master Material
+(function (angular) {
+    'use strict';
+    var app = angular.module("MasterMaterial", []);
+
+    app.controller("Material", ["$scope", "$http", function ($scope, $http) {
+        //    here $http get a information from "logs_with_gates.json"     
+
+        $http.get("materials.json")
+            //        then we will call then mathod to call function in which response argument gives all data contain in records as an Array 
+            .then(function (response) {
+
+                $scope.mydata = response.data;
+
+
+            });
+        $scope.quantity = 100;
+        $scope.myVar = false;
+        $scope.toggle = function () {
+            $scope.myVar = !$scope.myVar;
+        };
+        $scope.orderToDisplay = 10;
+        $scope.loadMore = function () {
+
+            console.log("im caliing");
+            if ($scope.orderToDisplay + 10 < $scope.data.length) {
+                $scope.orderToDisplay += 10;
+            } else {
+                $scope.orderToDisplay = $scope.data.length;
+            }
+        };
+        $scope.selected = {};
+        // gets the template to ng-include for a table row / item
+        $scope.getTemplate = function (data) {
+            if (data._id === $scope.selected._id) return 'edit';
+            else return 'display';
+        };
+
+        $scope.editMaterial = function (data) {
+            $scope.selected = angular.copy(data);
+        };
+
+        $scope.saveMaterial = function (idx) {
+            $scope.mydata[idx] = angular.copy($scope.selected);
+            $scope.reset();
+        };
+
+        $scope.reset = function () {
+            $scope.selected = {};
+        };
+
+    }]);
+    app.directive('whenScrolled', function () {
+
+        console.log("scrolling");
+        return function (scope, elm, attr) {
+            var raw = elm[0];
+            elm.bind('scroll', function () {
+                if (raw.scrollTop + raw.offsetHeight >= raw.scrollHeight) {
+                    scope.$apply(attr.whenScrolled);
+                }
+            });
+        };
+    });
 })(window.angular);
 
 
@@ -163,14 +326,14 @@ $(document).ready(function () {
 $(document).ready(function () {
     $('#upload').on('click', function () {
         $('#file').trigger('click');
-    })
-})
+    });
+});
 
 $(document).ready(function () {
     $('#upload1').on('click', function () {
         $('#file1').trigger('click');
-    })
-})
+    });
+});
 
 jQuery(function ($) {
     $('input[type="file"]').change(function () {
